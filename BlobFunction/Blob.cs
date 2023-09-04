b3dbdb3f13d83b69ac1dc5d4ffbbac5a88ff9ff7
nokorinotsubasa <alexsanderjuan13@gmail.com>
2023-09-04 11:43:53 -0300

    added code files

---
+++ BlobFunction/Blob.cs
@@ -0,0 +1,21 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace BlobFunction
+{
+    public class Blob
+    {
+        [FunctionName("CopyBlob")]
+
+        public async Task Run([BlobTrigger("data/{name}", Connection = "connectionString")]Stream streamBlob, [Blob("newdata/{name}", FileAccess.ReadWrite)] BlobClient newBlob, ILogger log)
+        {
+            await newBlob.UploadAsync(streamBlob);
+            log.LogInformation("Blob is copied");
+        }
+    }
+}
